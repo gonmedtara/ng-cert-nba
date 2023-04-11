48741ad756fdfcdc00c27b444c26a9f6cb45d03b
Mohamed GONTARA <HZ6247@engie.com>
2023-04-11 16:58:41 +0200

    ###############STEP-2#################

---
+++ src/app/components/modal-window/modal-window.component.ts
@@ -0,0 +1,22 @@
+import { Component, ElementRef, Input, OnChanges, SimpleChanges, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-window',
+  templateUrl: './modal-window.component.html',
+  styleUrls: ['./modal-window.component.css']
+})
+export class ModalWindowComponent implements OnChanges {
+  @ViewChild('modalHtml') modalHtml!: ElementRef;
+
+  @Input() isOpen!: boolean;
+
+  constructor(private _el: ElementRef) { }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['isOpen'] && changes['isOpen'].currentValue) {
+      this.modalHtml.nativeElement.show();
+    } else {
+      this.modalHtml.nativeElement.close();
+    }
+  }
+}
